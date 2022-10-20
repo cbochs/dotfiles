9ea16f3b65d70e6e845675cd75855b12599a3f0e
Calvin Bochulak <calvin.bochulak@gmail.com>
2022-10-20 12:37:52 -0600

    Add some git-blame keybindings to gitsigns.nvim

--- nvim/lua/core/plugins/gitsigns.lua
+++ nvim/lua/core/plugins/gitsigns.lua
@@ -6,10 +6,21 @@ local M = {}
 M.Details = function()
     return {
         "lewis6991/gitsigns.nvim",
-        config = function()
-            require("gitsigns").setup()
-        end,
+        config = M.Setup,
     }
+end
+
+M.Setup = function()
+    require("gitsigns").setup({
+        on_attach = function(bufnr)
+            local gitsigns = require("gitsigns")
+            local default_opts = { buffer = bufnr, silent = true }
+
+            vim.keymap.set("n", "<leader>gb", gitsigns.toggle_current_line_blame, default_opts)
+            vim.keymap.set("n", "<leader>gd", gitsigns.diffthis, default_opts)
+            vim.keymap.set("n", "<leader>gs", ":diffput<cr>", default_opts)
+        end
+    })
 end
 
 return M
