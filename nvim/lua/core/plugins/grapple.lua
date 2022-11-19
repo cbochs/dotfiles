42ad73815d79132bda8ac83864e332fd4e36c1a2
Calvin Bochulak <calvin.bochulak@gmail.com>
2022-11-18 23:02:13 -0700

    feat(nvim): play around with grapple's custom scope resolution

--- nvim/lua/core/plugins/grapple.lua
+++ nvim/lua/core/plugins/grapple.lua
@@ -15,7 +15,14 @@ M.Setup = function()
     local grapple = require("grapple")
 
     grapple.setup({
-        scope = "directory",
+        scope = function()
+            local root_files = vim.fs.find(".git", { upward = true })
+            if #root_files == 0 then
+                -- Fallback to grapple's default
+                return nil
+            end
+            return vim.fs.dirname(root_files[1])
+        end,
     })
 
     vim.keymap.set("n", "<leader>m", grapple.toggle, {})
