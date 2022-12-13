847c9c3c0724217b046e63fa17d503e60f4cd377
Calvin Bochulak <calvin.bochulak@gmail.com>
2022-12-13 16:55:24 -0700

    nvim: minor refactor of keymaps module

--- nvim/lua/core/keymaps/init.lua
+++ nvim/lua/core/keymaps/init.lua
@@ -4,75 +4,75 @@
 -- stylua: ignore start
 
 local keymap = vim.keymap.set
-local default_opts = { silent = true }
-local noremap_opts = { silent = true, noremap = true }
-local remap_opts = { silent = true, remap = true }
+local opts = { silent = true }
+local remap = { silent = true, remap = true }
 
 local NORMAL = "n"
 local VISUAL = "v"
 local INSERT = "i"
 local TERM   = "t"
+local ALL    = { NORMAL, VISUAL, INSERT }
 
 -- Leader keybindings
-keymap(NORMAL, "<leader>w", ":w<cr>",      default_opts) -- write file
-keymap(VISUAL, "<leader>y", '"+y',         default_opts) -- yank to clipboard
-keymap(NORMAL, "<leader>z", "<c-z>",       noremap_opts) -- suspend nvim
-keymap(NORMAL, "<leader>,", "",            default_opts) -- reload nvim
-keymap(NORMAL, "<leader>/", ":noh<cr>",    default_opts) -- clear search
-keymap(NORMAL, "<leader>e", ":Dirbuf<cr>", default_opts)
+keymap(NORMAL, "<leader>w", ":w<cr>",      opts) -- write file
+keymap(NORMAL, "<leader>z", "<c-z>",       opts) -- suspend nvim
+keymap(NORMAL, "<leader>,", "",            opts) -- reload nvim
+keymap(NORMAL, "<leader>/", ":noh<cr>",    opts) -- clear search
+keymap(NORMAL, "<leader>e", ":Dirbuf<cr>", opts)
 
-keymap(NORMAL, '<leader>"', 'ysiW"', remap_opts)
-keymap(NORMAL, "<leader>'", "ysiW'", remap_opts)
-keymap(NORMAL, "<leader>)", "ysiW)", remap_opts)
-keymap(NORMAL, "<leader>{", "ysiW{", remap_opts)
-keymap(NORMAL, "<leader>[", "ysiW[", remap_opts)
+-- Yank keybindings
+keymap(NORMAL, "<leader>y", '"+y',         opts)
+keymap(VISUAL, "<leader>y", '"+y',         opts)
+
+keymap(NORMAL, '<leader>"', 'ysiW"', remap)
+keymap(NORMAL, "<leader>'", "ysiW'", remap)
+keymap(NORMAL, "<leader>)", "ysiW)", remap)
+keymap(NORMAL, "<leader>{", "ysiW{", remap)
+keymap(NORMAL, "<leader>[", "ysiW[", remap)
 
 -- Janus-rails debug keybindings
-keymap(NORMAL, "<leader>dj", ":JanusRspec<cr>", default_opts)
+keymap(NORMAL, "<leader>dj", ":JanusRspec<cr>", opts)
 
 -- Toggle terminal keybindings
-keymap(TERM, "<c-j>", "<c-\\><c-n>", noremap_opts)
-keymap(NORMAL, "<leader>t", ":ToggleTerm<cr>", default_opts)
+keymap(TERM,   "<c-j>",     "<c-\\><c-n>",     opts)
+keymap(NORMAL, "<leader>t", ":ToggleTerm<cr>", opts)
 
 -- Escape keybindings
-keymap(INSERT, "<c-j>", "<esc>", default_opts)
-keymap(VISUAL, "<c-j>", "<esc>", default_opts)
+keymap(INSERT, "<c-j>", "<esc>", opts)
+keymap(VISUAL, "<c-j>", "<esc>", opts)
 
 -- Quit keybindings
 local quit = require("core.keymaps.quit")
-keymap(NORMAL, "<leader>q", quit.smart, default_opts) -- smart quit
-keymap(NORMAL, "<leader>Q", ":qa<cr>", default_opts) -- exit nvim
+keymap(NORMAL, "<leader>q", quit.smart, opts) -- smart quit
+keymap(NORMAL, "<leader>Q", ":qa<cr>",  opts) -- exit nvim
 
 -- Centring keybindings
-keymap(NORMAL, "<c-u>", "<c-u>zz", noremap_opts)
-keymap(NORMAL, "<c-d>", "<c-d>zz", noremap_opts)
+keymap(NORMAL, "<c-u>", "<c-u>zz", opts)
+keymap(NORMAL, "<c-d>", "<c-d>zz", opts)
 
 -- Window keybindings
-keymap(NORMAL, "<c-h>", "<c-w>h", noremap_opts)
-keymap(NORMAL, "<c-j>", "<c-w>j", noremap_opts)
-keymap(NORMAL, "<c-k>", "<c-w>k", noremap_opts)
-keymap(NORMAL, "<c-l>", "<c-w>l", noremap_opts)
-keymap(NORMAL, "<c-s-j>", ":split<cr>", default_opts)
-keymap(NORMAL, "<c-s-l>", ":vsplit<cr>", default_opts)
-
--- Buffer keybindings (DISABLED)
-keymap(NORMAL, "<c-b>h", ":bprevious<cr>", default_opts)
-keymap(NORMAL, "<c-b>l", ":bnext<cr>", default_opts)
+keymap(NORMAL, "<c-h>",   "<c-w>h",      opts)
+keymap(NORMAL, "<c-j>",   "<c-w>j",      opts)
+keymap(NORMAL, "<c-k>",   "<c-w>k",      opts)
+keymap(NORMAL, "<c-l>",   "<c-w>l",      opts)
+keymap(NORMAL, "<c-s-j>", ":split<cr>",  opts)
+keymap(NORMAL, "<c-s-l>", ":vsplit<cr>", opts)
 
 -- Utility keybindings
-keymap(NORMAL, "<cr>", "o<esc>", noremap_opts) -- add new line
-keymap(NORMAL, "U", "<c-r>", noremap_opts) -- redo
-keymap(NORMAL, "H", "_", noremap_opts) -- Go to: line beginning
-keymap(NORMAL, "L", "$", noremap_opts) -- Go to: line ending
-keymap(NORMAL, "x", '"_x', noremap_opts) -- blackhole single chars
-keymap(VISUAL, "<", "<gv", noremap_opts) -- indent line
-keymap(VISUAL, ">", ">gv", noremap_opts) -- outdent line
+keymap(NORMAL, "<cr>",  "o<esc>",        opts) -- add new line
+keymap(NORMAL, "U",     "<c-r>",         opts) -- redo
+keymap(NORMAL, "H",     "_",             opts) -- Go to: line beginning
+keymap(NORMAL, "L",     "$",             opts) -- Go to: line ending
+keymap(NORMAL, "x",     '"_x',           opts) -- blackhole single chars
+keymap(VISUAL, "<",     "<gv",           opts) -- indent line
+keymap(VISUAL, ">",     ">gv",           opts) -- outdent line
+keymap(VISUAL, "<s-j>", ":m '>+1<cr>gv", opts) -- move lin(s) down
+keymap(VISUAL, "<s-k>", ":m '<-2<cr>gv", opts) -- move line(s) up
 
 -- Disable arrow keys
-local all_modes = { NORMAL, VISUAL, INSERT }
-keymap(all_modes, "<left>", "", default_opts)
-keymap(all_modes, "<up>", "", default_opts)
-keymap(all_modes, "<down>", "", default_opts)
-keymap(all_modes, "<right>", "", default_opts)
+keymap(ALL, "<left>",  "", opts)
+keymap(ALL, "<up>",    "", opts)
+keymap(ALL, "<down>",  "", opts)
+keymap(ALL, "<right>", "", opts)
 
 -- stylua: ignore end
