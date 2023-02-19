920e03ccd9223226f83bca129af094dfbbfa6924
Calvin Bochulak <calvin.bochulak@gmail.com>
2023-02-19 00:24:54 -0700

    fish: export GPG_TTY

--- fish/conf.d/variables.fish
+++ fish/conf.d/variables.fish
@@ -6,3 +6,9 @@ set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
 # Preferred editor
 set -gx EDITOR "nvim"
 set -gx VISUAL "nvim"
+
+# GPG Setup
+# https://www.gnupg.org/(it)/documentation/manuals/gnupg/Common-Problems.html
+# https://stackoverflow.com/questions/39494631/gpg-failed-to-sign-the-data-fatal-failed-to-write-commit-object-git-2-10-0
+set -gx GPG_TTY (tty)
+
