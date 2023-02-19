8afcc16960fa3289d5e2b61b8fa2576feef314bf
Calvin Bochulak <calvin.bochulak@gmail.com>
2023-02-19 02:37:07 -0700

    fish: add reference to GPG_TTY

--- fish/conf.d/variables.fish
+++ fish/conf.d/variables.fish
@@ -8,7 +8,11 @@ set -gx EDITOR "nvim"
 set -gx VISUAL "nvim"
 
 # GPG Setup
-# https://www.gnupg.org/(it)/documentation/manuals/gnupg/Common-Problems.html
-# https://stackoverflow.com/questions/39494631/gpg-failed-to-sign-the-data-fatal-failed-to-write-commit-object-git-2-10-0
+# 
+# References:
+# - https://stackoverflow.com/questions/39494631/gpg-failed-to-sign-the-data-fatal-failed-to-write-commit-object-git-2-10-0
+# - https://unix.stackexchange.com/questions/388503/cant-sign-encrypt-unless-i-start-gpg-agent-manually
+# - https://www.gnupg.org/(it)/documentation/manuals/gnupg/Common-Problems.html
+# 
 set -gx GPG_TTY (tty)
 
