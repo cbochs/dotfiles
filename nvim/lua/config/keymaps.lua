e8900fb9099b72ab144517b2c6b42e6275f2c99f
Calvin Bochulak <calvin.bochulak@gmail.com>
2023-02-19 00:25:20 -0700

    nvim: group keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,10 +3,12 @@
 -- Add any additional keymaps here
 
 vim.keymap.set({ "i", "x" }, "<c-j>", "<esc>")
-vim.keymap.set("n", "U", "<c-r>")
-vim.keymap.set("n", ":", ";")
-vim.keymap.set("n", ";", ":")
 vim.keymap.set("t", "<c-j>", "<c-\\><c-n>")
+
+vim.keymap.set({ "n", "v" }, ":", ";")
+vim.keymap.set({ "n", "v" }, ";", ":")
+
+vim.keymap.set("n", "U", "<c-r>")
 
 local wk = require("which-key")
 wk.register({
