8c7f7fee1a83d65435bf60f62ffe6c36efe92ca3
Calvin Bochulak <calvin.bochulak@gmail.com>
2023-03-09 13:34:50 -0700

    nvim: use mini.trailspace + disable line numbers in terminal

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -3,36 +3,33 @@
 -- Add any additional autocmds here
 
 -- Inspired by https://github.com/cappyzawa/trim.nvim
-vim.api.nvim_create_augroup("TrimWhitespace", { clear = true })
+
 vim.api.nvim_create_autocmd("BufWritePre", {
-    group = "TrimWhitespace",
+    group = vim.api.nvim_create_augroup("TrimWhitespace", { clear = true }),
     pattern = "*",
     callback = function()
-        local patterns = {
-            [[%s/\s\+$//e]], -- remove unwanted spaces
-            [[%s/\($\n\s*\)\+\%$//]], -- trim last line
-            [[%s/\%^\n\+//]], -- trim first line
-        }
-
-        local save = vim.fn.winsaveview()
-        for _, pattern in ipairs(patterns) do
-            vim.api.nvim_exec(string.format("keepjumps keeppatterns silent! %s", pattern), false)
-        end
-        vim.fn.winrestview(save)
+        MiniTrailspace.trim()
     end,
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-    pattern = { "ruby" },
+    pattern = { "bash", "ruby" },
     callback = function()
         vim.b.autoformat = false
     end,
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-    pattern = { "go", "bash" },
+    pattern = { "bash", "go" },
     callback = function()
         vim.b.expandtab = false
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
+    callback = function()
+        vim.opt_local.number = false
+        vim.opt_local.relativenumber = false
     end,
 })
 
