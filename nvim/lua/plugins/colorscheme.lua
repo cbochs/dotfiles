fbde18de9e5571f796159084bc09ce8e53483d56
Calvin Bochulak <calvin.bochulak@gmail.com>
2023-03-10 15:43:47 -0700

    nvim: add some cool suggestions to kanagawa opts

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,21 @@ return {
         opts = {
             compile = true,
             dimInactive = true,
+            colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
+
+            ---@type fun(colors: KanagawaColors): table
+            overrides = function(colors)
+                local theme = colors.theme
+                return {
+                    TelescopeTitle = { fg = theme.ui.special, bold = true },
+                    TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+                    TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+                    TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+                    TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+                    TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+                    TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+                }
+            end,
         },
     },
     {
