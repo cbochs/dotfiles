e8276c7358d588a9e001c19afa09dabd01b72e37
Calvin Bochulak <calvin.bochulak@gmail.com>
2023-03-28 17:51:09 -0600

    nvim: add nvim-spider

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -129,6 +129,7 @@ return {
             },
         },
         opts = {
+            wrap = true,
             select_first = true,
             escape = {
                 ["<c-j>"] = true,
@@ -173,5 +174,16 @@ return {
                 return { "treesitter", "indent" }
             end,
         },
+    },
+
+    {
+        -- stylua: ignore
+        keys = {
+            { "w", function() require("spider").motion("w") end, mode = { "n", "o", "x" } },
+            { "b", function() require("spider").motion("b") end, mode = { "n", "o", "x" } },
+            { "e", function() require("spider").motion("e") end, mode = { "n", "o", "x" } },
+            { "ge", function() require("spider").motion("ge") end, mode = { "n", "o", "x" } },
+        },
+        "chrisgrieser/nvim-spider",
     },
 }
