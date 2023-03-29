da9654a3efe9eef2296af7923d672e945eac6286
Calvin Bochulak <calvin.bochulak@gmail.com>
2023-03-29 08:31:49 -0600

    fish: set lazygit config file manually

--- fish/conf.d/variables.fish
+++ fish/conf.d/variables.fish
@@ -7,12 +7,15 @@ set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
 set -gx EDITOR "nvim"
 set -gx VISUAL "nvim"
 
+# Lazygit config
+set -gx LG_CONFIG_FILE $HOME/.config/lazygit/config.yml
+
 # GPG Setup
-# 
+#
 # References:
 # - https://stackoverflow.com/questions/39494631/gpg-failed-to-sign-the-data-fatal-failed-to-write-commit-object-git-2-10-0
 # - https://unix.stackexchange.com/questions/388503/cant-sign-encrypt-unless-i-start-gpg-agent-manually
 # - https://www.gnupg.org/(it)/documentation/manuals/gnupg/Common-Problems.html
-# 
+#
 set -gx GPG_TTY (tty)
 
