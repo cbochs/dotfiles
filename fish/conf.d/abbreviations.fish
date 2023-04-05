14e1a9eab9b6818dd85984b29f258245661720df
Calvin Bochulak <calvin.bochulak@gmail.com>
2023-04-04 20:24:48 -0600

    fish: remove deprecated (and redundant) '-U' flag for shell abbreviations

--- fish/conf.d/abbreviations.fish
+++ fish/conf.d/abbreviations.fish
@@ -1,42 +1,42 @@
-abbr -a -U ll  exa -la
+abbr -a ll  exa -la
 
-abbr -a -U b bat
-abbr -a -U c cd
-abbr -a -U f fg
-abbr -a -U l exa
-abbr -a -U n nvim
-abbr -a -U v nvim
-abbr -a -U s ssh
+abbr -a b bat
+abbr -a c cd
+abbr -a f fg
+abbr -a l exa
+abbr -a n nvim
+abbr -a v nvim
+abbr -a s ssh
 
-abbr -a -U nvim-copilot NVIM_APPNAME=nvim-copilot nvim
+abbr -a nvim-copilot NVIM_APPNAME=nvim-copilot nvim
 
-abbr -a -U ze zellij
-abbr -a -U lg lazygit
-abbr -a -U dk lazydocker
+abbr -a ze zellij
+abbr -a lg lazygit
+abbr -a dk lazydocker
 
-abbr -a -U g   git
-abbr -a -U gb  git branch
-abbr -a -U gc  git checkout
-abbr -a -U gl  git log
-abbr -a -U gm  git submodule update --recursive
-abbr -a -U gp  git push
-abbr -a -U gP  git pull
-abbr -a -U gpu git pull upstream master
-abbr -a -U gcm git checkout master
-abbr -a -U gr  git remote
-abbr -a -U gs  git st
+abbr -a g   git
+abbr -a gb  git branch
+abbr -a gc  git checkout
+abbr -a gl  git log
+abbr -a gm  git submodule update --recursive
+abbr -a gp  git push
+abbr -a gP  git pull
+abbr -a gpu git pull upstream master
+abbr -a gcm git checkout master
+abbr -a gr  git remote
+abbr -a gs  git st
 
-abbr -a -U mk minikube
-abbr -a -U mi minikube
-abbr -a -U k  kubectl
-abbr -a -U kc kubectl config
-abbr -a -U kg kubectl get
-abbr -a -U kl kubectl logs --tail=10
+abbr -a mk minikube
+abbr -a mi minikube
+abbr -a k  kubectl
+abbr -a kc kubectl config
+abbr -a kg kubectl get
+abbr -a kl kubectl logs --tail=10
 
-abbr -a -U d  docker
-abbr -a -U di docker image
-abbr -a -U dc docker compose
-abbr -a -U dv docker volume
+abbr -a d  docker
+abbr -a di docker image
+abbr -a dc docker compose
+abbr -a dv docker volume
 
-abbr -a -U a asdf exec
-abbr -a -U i asdf exec irb
+abbr -a a asdf exec
+abbr -a i asdf exec irb
