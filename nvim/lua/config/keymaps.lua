6dc9368df88fd46782247d66b5d44bc1247a4ae7
Calvin Bochulak <calvin.bochulak@gmail.com>
2023-08-21 17:30:22 -0600

    nvim: experiment with wezterm

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("n", "<leader>tl", require("config.wezterm").test.last)
+vim.keymap.set("n", "<leader>tn", require("config.wezterm").test.near)
+vim.keymap.set("n", "<leader>tf", require("config.wezterm").test.file)
+vim.keymap.set("n", "<leader>ua", require("config.wezterm").lazygit)
+
 vim.keymap.set({ "i", "x" }, "<c-j>", "<esc>")
 vim.keymap.set("t", "<c-j>", "<c-\\><c-n>")
 
@@ -10,8 +15,6 @@ vim.keymap.set({ "n", "v" }, ";", ":")
 
 vim.keymap.set("n", "U", "<c-r>")
 
-vim.keymap.set("n", "<leader>utj", "<cmd>split | terminal<cr>a", { desc = "Toggle terminal (down)" })
-vim.keymap.set("n", "<leader>utl", "<cmd>vsplit | terminal<cr>a", { desc = "Toggle terminal (right)" })
 vim.keymap.set("n", "<leader>up", "<cmd>InspectTree<cr>", { desc = "Toggle treesitter" })
 
 local wk = require("which-key")
---
+++ nvim/lua/config/wezterm.lua
@@ -0,0 +1,41 @@
+local Wezterm = {}
+
+Wezterm.run = function(...)
+    vim.fn.system(("nvim-wezterm.sh %s"):format(table.concat({ ... }, " ")))
+end
+
+Wezterm.lazygit = function()
+    Wezterm.run("lazygit")
+end
+
+Wezterm.test = {}
+Wezterm.test.history = {}
+Wezterm.test.run = function(file_name, line_number)
+    Wezterm.test.history.file_name = file_name
+    Wezterm.test.history.line_number = line_number
+
+    if line_number then
+        Wezterm.run(("test %s %s"):format(file_name, line_number))
+    else
+        Wezterm.run(("test %s %s"):format(file_name))
+    end
+end
+
+Wezterm.test.file = function()
+    Wezterm.run("test", vim.fn.expand("%"))
+end
+
+Wezterm.test.near = function()
+    Wezterm.run("test", vim.fn.expand("%"), vim.fn.line("."))
+end
+
+Wezterm.test.last = function()
+    if not Wezterm.test.history.file_name then
+        vim.notify("No test run yet.")
+        return
+    end
+
+    Wezterm.run("test", Wezterm.last_run.file_name, Wezterm.last_run.line_number)
+end
+
+return Wezterm
