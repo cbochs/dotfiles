def418fb121ed2619daac6fde21cfcb262cfad65
Calvin Bochulak <calvin.bochulak@gmail.com>
2023-08-23 14:46:13 -0600

    fish: update fish abbreviations

--- fish/conf.d/abbreviations.fish
+++ fish/conf.d/abbreviations.fish
@@ -1,4 +1,4 @@
-abbr -a ll  exa -la
+abbr -a nvim-copilot NVIM_APPNAME=nvim-copilot nvim
 
 abbr -a b bat
 abbr -a c cd
@@ -8,35 +8,22 @@ abbr -a n nvim
 abbr -a v nvim
 abbr -a s ssh
 
-abbr -a nvim-copilot NVIM_APPNAME=nvim-copilot nvim
-
-abbr -a ze zellij
+abbr -a ll exa -la
 abbr -a lg lazygit
-abbr -a dk lazydocker
 
 abbr -a g   git
 abbr -a gb  git branch
 abbr -a gc  git checkout
-abbr -a gl  git log
-abbr -a gm  git submodule update --recursive
-abbr -a gp  git push
-abbr -a gP  git pull
-abbr -a gpu git pull upstream master
 abbr -a gcm git checkout master
+abbr -a gl  git log
+abbr -a gP  git push
+abbr -a gp  git pull
+abbr -a gpu git pull upstream master
 abbr -a gr  git remote
 abbr -a gs  git st
-
-abbr -a mk minikube
-abbr -a mi minikube
-abbr -a k  kubectl
-abbr -a kc kubectl config
-abbr -a kg kubectl get
-abbr -a kl kubectl logs --tail=10
+abbr -a gsm git submodule update --recursive
 
 abbr -a d  docker
 abbr -a di docker image
 abbr -a dc docker compose
 abbr -a dv docker volume
-
-abbr -a a asdf exec
-abbr -a i asdf exec irb
