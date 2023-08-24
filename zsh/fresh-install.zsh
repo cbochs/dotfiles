b55d71185cc135805eaa647d0f1d57f6664c52a4
Calvin Bochulak <calvin@Calvins-MBP.lan>
2023-08-24 10:19:13 -0600

    zsh: change suffix of fresh-install to zsh

--- zsh/fresh-install.zsh
+++ zsh/fresh-install.zsh
@@ -1,11 +1,13 @@
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
 
 ln -sf $HOME/.config/zsh/.zshenv $HOME/.zshenv
+
+source $HOME/.zshenv
 
 rm -rf $ZPLUGINDIR
 mkdir  $ZPLUGINDIR
 
-for plugin in $ZPLUGINS
+for plugin in $(echo $ZPLUGINS)
 do
 	plugin_user=$(echo $plugin | awk -F/ '{ print $1 }')
 	plugin_name=$(echo $plugin | awk -F/ '{ print $2 }')
@@ -13,3 +15,5 @@ do
 
 	git clone $plugin_repo $ZPLUGINDIR/$plugin_name --single-branch --depth 1
 done
+
+source $ZDOTDIR/.zshrc
