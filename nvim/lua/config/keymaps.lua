23c163918ffd5e799491b561416b98eb41a912a0
Calvin Bochulak <calvin.bochulak@gmail.com>
2024-01-28 15:48:38 -0700

    nvim: simplify wezterm keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,12 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>tl", require("config.wezterm").test.last, { desc = "Test last" })
-vim.keymap.set("n", "<leader>tn", require("config.wezterm").test.near, { desc = "Test nearest" })
-vim.keymap.set("n", "<leader>tt", require("config.wezterm").test.file, { desc = "Test file" })
-vim.keymap.set("n", "<leader>jj", require("config.wezterm").lazygit, { desc = "Open lazygit" })
-vim.keymap.set("n", "<leader>gho", require("config.wezterm").github_open, { desc = "Open in Github" })
-vim.keymap.set("n", "<leader>ghl", require("config.wezterm").github_link, { desc = "Copy Github link" })
+local Wezterm = require("config.wezterm")
+vim.keymap.set("n", "<leader>tl", Wezterm.test.last, { desc = "Test last" })
+vim.keymap.set("n", "<leader>tn", Wezterm.test.near, { desc = "Test nearest" })
+vim.keymap.set("n", "<leader>tt", Wezterm.test.file, { desc = "Test file" })
+vim.keymap.set("n", "<leader>jj", Wezterm.lazygit, { desc = "Open lazygit" })
+vim.keymap.set("n", "<leader>gho", Wezterm.github_open, { desc = "Open in Github" })
+vim.keymap.set("n", "<leader>ghl", Wezterm.github_link, { desc = "Copy Github link" })
 
 vim.keymap.set({ "i", "x" }, "<c-j>", "<esc>")
 
