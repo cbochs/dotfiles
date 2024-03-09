2eff89ced366e44f9c942594ee30d8a11d3e5801
Calvin Bochulak <calvin@Calvins-MBP.lan>
2024-03-09 10:49:25 -0700

    nix: clean up nix-darwin

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -12,27 +12,22 @@
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
-      environment.systemPackages = [ pkgs.vim ];
+      environment.systemPackages = [
+          pkgs.vim
+      ];
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
+      nix.settings.experimental-features = "nix-command flakes"; # Required for flakes
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
-
-      # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
+
+      programs.zsh.enable = true; # Create /etc/zshrc
+
+      security.pam.enableSudoTouchIdAuth = true;
+
+      services.nix-daemon.enable = true; # Auto upgrade nix package and daemon service
+
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+      system.stateVersion = 4;
     };
   in
   {
