318fe02bbe3a00d7cf2d0dd2f8d1f343bb7394ff
Calvin Bochulak <calvin@Calvins-MBP.lan>
2024-03-09 10:54:07 -0700

    nvim: sort disabled plugins

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,8 +1,6 @@
 return {
     { "catppuccin", enabled = false },
-    { "SmiteshP/nvim-navic", enabled = false },
-    { "folke/tokyonight.nvim", enabled = false },
-
-    -- Prefer nvim-surround
     { "echasnovski/mini.surround", enabled = false },
+    { "folke/tokyonight.nvim", enabled = false },
+    { "SmiteshP/nvim-navic", enabled = false },
 }
