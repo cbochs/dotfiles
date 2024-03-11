20c2aa0994e3033ac414746e1e8c1e9ecf44b418
Calvin Bochulak <calvin.bochulak@gmail.com>
2024-03-11 15:09:29 -0600

    nvim: cleanup nvim options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,13 +6,18 @@ vim.opt.cmdwinheight = 1
 
 -- Folding (nvim-ufo)
 vim.opt.foldcolumn = "1"
+vim.opt.foldenable = true
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
-vim.opt.foldenable = true
 
 -- Jumplist (portal.nvim)
 vim.opt.jumpoptions = "stack"
 
--- Disable comment continuation
+-- Formatting options
+-- See:
+--   :h formatoptions
+--   :h fo-table
+--   :h textwidth
+vim.opt.textwidth = 80
 vim.opt.formatoptions:remove("o") -- Don't continue after 'o' or 'O'
 vim.opt.formatoptions:remove("r") -- Don't continue after <cr>
