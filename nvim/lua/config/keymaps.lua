ff5f01db965ff41f7597a614637cdac62c2e2035
Calvin Bochulak <calvin.bochulak@gmail.com>
2024-03-11 10:32:53 -0600

    nvim: cleanup lua/config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,7 +1,3 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
 local Wezterm = require("config.wezterm")
 vim.keymap.set("n", "<leader>tl", Wezterm.test.last, { desc = "Test last" })
 vim.keymap.set("n", "<leader>tn", Wezterm.test.near, { desc = "Test nearest" })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,16 +1,8 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 
 -- Reduce command buffer size
 vim.opt.cmdwinheight = 1
-
--- Conceal (Neorg)
--- vim.opt.conceallevel = 2
--- vim.opt.concealcursor = "nc"
 
 -- Folding (nvim-ufo)
 vim.opt.foldcolumn = "1"
