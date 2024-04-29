b20dd3a411e6fd342a90903ba4f9d922e49b5a1c
Calvin Bochulak <calvin.bochulak@gmail.com>
2024-04-29 16:16:24 -0600

    nvim: use ruby_lsp instead of ruby_ls

--- nvim/lua/plugins/ruby.lua
+++ nvim/lua/plugins/ruby.lua
@@ -12,36 +12,7 @@ return {
         "neovim/nvim-lspconfig",
         opts = {
             servers = {
-                ruby_ls = { mason = false },
-            },
-        },
-    },
-
-    {
-        "mfussenegger/nvim-dap",
-        optional = true,
-        dependencies = {
-            "suketa/nvim-dap-ruby",
-            config = function()
-                require("dap-ruby").setup()
-            end,
-        },
-    },
-
-    {
-        "nvim-neotest/neotest",
-        optional = true,
-        dependencies = {
-            "olimorris/neotest-rspec",
-        },
-        opts = {
-            adapters = {
-                ["neotest-rspec"] = {
-                    -- NOTE: By default neotest-rspec uses the system wide rspec gem instead of the one through bundler
-                    rspec_cmd = function()
-                        return { "docker", "compose", "exec", "portal", "bin/rspec" }
-                    end,
-                },
+                ruby_lsp = { mason = false },
             },
         },
     },
